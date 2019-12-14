b19782f86df0301f2d7f2c11f6f1e688e0f7a8e9
boljshoj <123qwepioneeraZd>
2019-12-14 19:36:27 +0300

    update Test

--- src/main/java/com/ifmo/lesson24/BQueue.java
+++ src/main/java/com/ifmo/lesson24/BQueue.java
@@ -4,49 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BQueue<T> {
-    private final List<T> queue;
+    private List<T> queue = new ArrayList<>();
     private int size;
 
     public BQueue(int size) {
-        this.queue = new ArrayList<T>(size);
         this.size = size;
     }
 
-    public void put(T t){
-        while (true){
-            synchronized (queue){
-                while (queue.size() == size){
-                    try {
-                        System.out.println("Queue is full");
-                        queue.wait();
-                    } catch (InterruptedException e){
-                        e.printStackTrace();
-                        Thread.currentThread().interrupt();
-                    }
-                }
-                queue.add(t);
-                System.out.println(Thread.currentThread().getName() + " add " + t);
-                queue.notify();
-            }
+    public synchronized void put(T t) throws InterruptedException{
+        while (this.queue.size() == this.size){
+            System.out.println("Queue is full");
+            wait();
         }
+        if (this.queue.size() == 0){
+            notifyAll();
+        }
+        this.queue.add(t);
     }
 
-    public T take() {
+    public synchronized T take() throws InterruptedException{
         T result;
-        synchronized (queue) {
-                while (queue.size() == 0) {
-                    System.out.println("Queue is empty");
-                    try {
-                        queue.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        Thread.currentThread().interrupt();
-                    }
-                }
-                result = queue.remove(0);
-                System.out.println(Thread.currentThread().getName() + " take " + result);
-                queue.notify();
-                return result;
-        }
+            while (this.queue.size() == 0) {
+                System.out.println("Queue is empty");
+                wait();
+            }
+            if (this.queue.size() == this.size) {
+                notifyAll();
+            }
+        result = queue.remove(0);
+        return result;
     }
 }
--- src/main/java/com/ifmo/lesson24/Main.java
+++ src/main/java/com/ifmo/lesson24/Main.java
@@ -1,16 +1,65 @@
 package com.ifmo.lesson24;
 
+import java.util.Random;
+
 public class Main {
-    static BQueue<Integer> queue = new BQueue<>(10);
+    public static void main(String[] args) throws InterruptedException {
+        BQueue queue = new BQueue(5);
+        new Thread(new Producer(queue)).start();
+        Thread.currentThread().sleep(1000);
+        new Thread(new Consumer(queue)).start();
+    }
 
-    public static void main(String[] args) {
-        Thread worker = new Thread(() -> queue.take());
-        Thread worker2 = new Thread(() -> queue.take());
+    static class Producer implements Runnable {
+        private final BQueue queue;
 
-        Thread producer = new Thread(() -> queue.put(1));
+        public Producer(BQueue queue) {
+            this.queue = queue;
+        }
 
-        worker.start();
-        worker2.start();
-        producer.start();
+        @Override
+        public void run() {
+            System.out.println("[Producer] run");
+            while (true) {
+                try {
+                    queue.put(produce());
+                    Thread.currentThread().sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private Integer produce() {
+            Integer i = new Random().nextInt(100);
+            System.out.println("[Producer] produce: " + i);
+            return i;
+        }
+    }
+
+    static class Consumer implements Runnable {
+        private final BQueue queue;
+
+        public Consumer(BQueue queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("[Consumer] run");
+            while (true) {
+                try {
+                    consume();
+                    Thread.currentThread().sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void consume() throws InterruptedException {
+            Integer i = (int) queue.take();
+            System.out.println("[Consumer] consumed: " + i);
+        }
     }
 }
